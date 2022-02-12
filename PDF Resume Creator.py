e20cbaec6722964ab5da6fc87e2a26c573758d74
Mary Gwen Susa <Mary Gwen G. Susa>
2022-02-12 22:23:49 +0800

    Add an image

--- PDF Resume Creator.py
+++ PDF Resume Creator.py
@@ -1,10 +1,11 @@
 import os as access; from fpdf import FPDF; import json
-
 # import OS to interact with the Microsoft Windows default settings
 # fpdf module to generate pdf
-
 
 data = open("resume.json")
 data = json.load(data) # read the json file
 susaPDF = FPDF('P', 'mm', 'Legal') # orientation, measure, size of page; set up the pdf
 susaPDF.add_page()
+
+def layout():
+    susaPDF.image('2by2.jpg', 165, 10, 40, 0)  #space and dimensions   
